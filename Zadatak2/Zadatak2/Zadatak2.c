6d3e41ae7d8a5134408ce57315bc0736b6e37dc7
luka-skoko <luka.skoko@gmail.com>
2024-11-02 13:36:03 +0100

    Everyting works apart from read_from_file, still have to add sort_list

--- Zadatak2/Zadatak2/Zadatak2.c
+++ Zadatak2/Zadatak2/Zadatak2.c
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define ERROR_WHILE_OPENING_FILE 1
+
 typedef struct _person* position;
 typedef struct _person
 {
@@ -20,12 +22,19 @@ int insert_after(position previous, position to_);
 void print_list(position first);
 position find_last(position head);
 position find_by_lname(position first, char* lname);
-//position find_previous();
+position find_previous(position first, char* lname);
+int erase_person(position first, char* lname);
+int append_person(position head, char* specific_lname, char* fname, char* lname, int birth_year);
+int prepend_person(position head, char* specific_lname, char* fname, char* lname, int birth_year);
+int write_in_file(position first);
+int read_from_file(char* filename);
+
+int sort_list(position first);
 
 int main()
 {
     person head = {"","",0, NULL};
-    person *searchedName;
+    position searchedName, previous; 
 
     prepend_list(&head, "Leo", "Pavlovic", 1998);
     prepend_list(&head, "Lucija", "Pavlic", 1998);
@@ -34,8 +43,24 @@ int main()
     append_list(&head, "Mate", "Matic", 1980);
     print_list(head.next);
 
-    searchedName = find_by_lname(head.next, "Pavlovic");
+    /*searchedName = find_by_lname(head.next, "Pavlovic");
     printf("Trazena osoba: %s %s\n", searchedName->fname, searchedName->lname);
+
+    previous = find_previous(head.next, "Pavlovic");
+    printf("Prethodnik: %s %s\n\n", previous->fname, previous->lname);*/
+    printf("\n");
+    erase_person(head.next, "Pavlovic");
+
+    append_person(&head, "Baric", "Jure", "Matkovic", 1997);
+    print_list(head.next);
+    printf("\n");
+
+    prepend_person(&head, "Matkovic", "Luka", "Barisic", 2002);
+    print_list(head.next);
+
+    write_in_file(head.next);
+    read_from_file("lista.txt");
+    print_list(head.next);
 }
 
 position create_person(char* fname, char* lname, int birth_year)
@@ -84,6 +109,7 @@ int append_list(position head, char* fname, char* lname, int birth_year)
     //new_person = last->next;
     //last->next = new_person;
     insert_after(last, new_person);
+    return EXIT_SUCCESS;
 
 }
 
@@ -102,7 +128,7 @@ position find_last(position head)
 {
     position temp = head;
 
-    while (temp->next)
+    while(temp->next)
     {
         temp = temp->next;
     }
@@ -114,6 +140,7 @@ int insert_after(position previous, position to_)
 {
     to_->next = previous->next;
     previous->next = to_;
+    return EXIT_SUCCESS;
 }
 
 position find_by_lname(position first, char* lname)
@@ -129,6 +156,105 @@ position find_by_lname(position first, char* lname)
         temp = temp->next;
     }
     return NULL;
+}
+
+position find_previous(position first, char* lname)
+{
+    position temp = first;
+
+    while(temp->next)
+    {
+        if (strcmp(temp->next->lname, lname) == 0)
+            return temp;
+        temp = temp->next;
+    }
+    return NULL;
+}
+
+int erase_person(position first, char* lname)
+{
+    position previous = find_previous(first, lname);
+    position temp = previous->next;
+
+    previous->next = previous->next->next;
+    free(temp);
+    return EXIT_SUCCESS;
+}
+
+int append_person(position head, char* specific_lname, char* fname, char* lname, int birth_year)
+{
+    position new_person = NULL, specific;
+
+    new_person = create_person(fname, lname, birth_year);
+    if (!new_person)
+        return -1;
+    
+    specific = find_by_lname(head->next, specific_lname);
+    new_person->next = specific->next;
+    specific->next = new_person;
+    return EXIT_SUCCESS;
+    
+}
+
+int prepend_person(position head, char* specific_lname, char* fname, char* lname, int birth_year)
+{
+    position new_person = NULL, previous;
+
+    new_person = create_person(fname, lname, birth_year);
+    if (!new_person)
+        return -1;
+
+    previous = find_previous(head->next, specific_lname);
+    new_person->next = previous->next;
+    previous->next = new_person;
+    return EXIT_SUCCESS;
+}
+
+int write_in_file(position first)
+{
+    FILE* fp = NULL;
+    position temp = first;
+
+    fp = fopen("lista.txt", "w");
+    
+    if (fp == NULL)
+    {
+        printf("Error while opening file!");
+        return ERROR_WHILE_OPENING_FILE;
+    }
+        
+
+    while (temp)
+    {
+        fprintf(fp, "First name: %s\nLast name: %s\nBirth year: %d\n\n", temp->fname, temp->lname, temp->birth_year);
+        temp = temp->next;
+    }
+
+    fclose(fp);
+    return EXIT_SUCCESS;
+}
+
+int read_from_file(char* filename)
+{
+    FILE* fp = NULL;
+    person new_head = { "","",0, NULL };
+    char* buffer = "", current_fname, current_lname;
+    int current_birth_year;
+
+    fp = fopen(filename, "r");
+
+    if (fp == NULL)
+    {
+        printf("Error while opening file!");
+        return ERROR_WHILE_OPENING_FILE;
+    }
+
+    while (!feof(fp))
+    {
+        fscanf(fp, "%s", buffer);
+        sscanf(buffer, "%s %s %d", current_fname, current_lname, &current_birth_year);
+        append_list(&new_head, current_fname, current_lname, current_birth_year);
+    }
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
