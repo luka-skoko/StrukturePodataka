3e949f1523556e432f8a1169e3a0b41b2b418da5
luka-skoko <luka.skoko@gmail.com>
2024-11-05 11:35:19 +0100

    everything works

--- Zadatak2/Zadatak2/Zadatak2.c
+++ Zadatak2/Zadatak2/Zadatak2.c
@@ -5,11 +5,13 @@
 #include <stdlib.h>
 
 #define ERROR_WHILE_OPENING_FILE 1
+#define MAX_NAME_SIZE 32
+#define MAX_FILE_NAME_SIZE 100
 
 typedef struct _person* position;
 typedef struct _person
 {
-    char fname[32], lname[32];
+    char fname[MAX_NAME_SIZE], lname[MAX_NAME_SIZE];
     int birth_year;
     position next;
 
@@ -25,26 +27,28 @@ position find_by_lname(position first, char* lname);
 position find_previous(position first, char* lname);
 int erase_person(position first, char* lname);
 int append_person(position head, char* specific_lname, char* fname, char* lname, int birth_year);
-int prepend_person(position head, char* specific_lname, char* fname, char* lname, int birth_year);
+int prepend_person(position first, char* specific_lname, char* fname, char* lname, int birth_year);
 int write_in_file(position first);
 int read_from_file(char* filename);
-
-int sort_list(position first);
+int sort_list(position head);
 
 int main()
 {
     person head = {"","",0, NULL};
-    char fname[32], lname[32];
+    char fname[MAX_NAME_SIZE], lname[MAX_NAME_SIZE], specific_lname[MAX_NAME_SIZE];
     int birth_year;
     int odabir;
+    position searchedName;
+    char filename[MAX_FILE_NAME_SIZE];
+
     printf("Odaberi koju funkciju zelis:\n");
-    printf("0 - zatvori izbornik");
+    printf("0 - zatvori izbornik\n");
     printf("1 - dinamicki dodaje novi element na pocetak liste\n");
     printf("2 - ispisuje listu\n");
     printf("3 - dinamicki dodaje novi element na kraj liste\n");
     printf("4 - pronalazi element u listi(po prezimenu)\n");
     printf("5 - brise odredjeni element iz liste\n");
-    printf("6 - dinamicki dodaje novi element iza odreðenog elementa\n");
+    printf("6 - dinamicki dodaje novi element iza odredjenog elementa\n");
     printf("7 - dinamicki dodaje novi element ispred odredjenog elementa\n");
     printf("8 - sortira listu po prezimenima osoba\n");
     printf("9 - upisuje listu u datoteku\n");
@@ -57,53 +61,80 @@ int main()
         {
             case 0:
                 break;
+
             case 1:
-                printf("\nUnesi ime, prezime i datum rodjenja osobe koju zelis dodati:\n");
+                printf("Unesi ime, prezime i godinu rodjenja osobe koju zelis dodati:\n");
                 scanf("%s", fname);
                 scanf("%s", lname);
                 scanf("%d", &birth_year);
                 prepend_list(&head, fname, lname, birth_year);
                 break;
+
             case 2:
                 print_list(head.next);
                 break;
+
             case 3:
-                printf("\nUnesi ime, prezime i datum rodjenja osobe koju zelis dodati:\n");
+                printf("Unesi ime, prezime i godinu rodjenja osobe koju zelis dodati:\n");
                 scanf("%s", fname);
                 scanf("%s", lname);
                 scanf("%d", &birth_year);
                 append_list(&head, fname, lname, birth_year);
                 break;
+
             case 4:
                 printf("Unesi prezime osobe koju trazis:\n");
                 scanf("%s", lname);
+                searchedName = find_by_lname(head.next, lname);
+                printf("Trazena osoba: %s %s %d", searchedName->fname, searchedName->lname, searchedName->birth_year);
+                break;
+
+            case 5:
+                printf("Unesi prezime osobe koju zelis izbrisati:\n");
+                scanf("%s", lname);
+                erase_person(head.next, lname);
+                break;
+
+            case 6:
+                printf("Unesi ime, prezime i godinu rodjenja osobe koju zelis dodati : \n");
+                scanf("%s", fname);
+                scanf("%s", lname);
+                scanf("%d", &birth_year);
+                printf("Unesi prezime osobe iza koje zelis dodati osobu:\n");
+                scanf("%s", specific_lname);
+                append_person(&head, specific_lname, fname, lname, birth_year);
+                break;
+
+            case 7:
+                printf("Unesi ime, prezime i godinu rodjenja osobe koju zelis dodati : \n");
+                scanf("%s", fname);
+                scanf("%s", lname);
+                scanf("%d", &birth_year);
+                printf("Unesi prezime osobe ispred koje zelis dodati osobu:\n");
+                scanf("%s", specific_lname);
+                prepend_person(&head, specific_lname, fname, lname, birth_year);
+                break;
+
+            case 8:
+                sort_list(&head);
+                break;
+
+            case 9:
+                write_in_file(head.next);
+                printf("Upisivanje u file odradjeno!\n");
+                break;
+
+            case 10:
+                printf("Unesi ime datoteke iz koje zelis procitati listu:\n");
+                scanf("%s", filename);
+                read_from_file(filename);
+                break;
+
         }
 
     } while (odabir != 0);
         
-    /*position searchedName, previous;
-
-    prepend_list(&head, "Leo", "Pavlovic", 1998);
-    prepend_list(&head, "Lucija", "Pavlic", 1998);
-    append_list(&head, "Marko", "Bulic", 2006);
-    prepend_list(&head, "Ante", "Baric", 2001);
-    append_list(&head, "Mate", "Matic", 1980);
-    print_list(head.next);
-
-    printf("\n");
-    erase_person(head.next, "Pavlovic");
-
-    append_person(&head, "Baric", "Jure", "Matkovic", 1997);
-    print_list(head.next);
-    printf("\n");
-
-    prepend_person(&head, "Matkovic", "Luka", "Barisic", 2002);
-    print_list(head.next);
-
-    write_in_file(head.next);
-    read_from_file("lista.txt");
-    printf("\nProcitano iz filea:\n");
-    print_list(head.next);*/
+    return 0;
 }
 
 position create_person(char* fname, char* lname, int birth_year)
@@ -222,7 +253,6 @@ int erase_person(position first, char* lname)
 
     previous->next = previous->next->next;
     free(temp);
-    return EXIT_SUCCESS;
 }
 
 int append_person(position head, char* specific_lname, char* fname, char* lname, int birth_year)
@@ -248,9 +278,36 @@ int prepend_person(position head, char* specific_lname, char* fname, char* lname
     if (!new_person)
         return -1;
 
-    previous = find_previous(head->next, specific_lname);
+    previous = find_previous(head, specific_lname);
     new_person->next = previous->next;
     previous->next = new_person;
+    return EXIT_SUCCESS;
+}
+
+int sort_list(position head)
+{
+    position current, previous, temp, end;
+    end = NULL;
+
+    while (head->next != end) 
+    {
+        previous = head;
+        current = head->next;
+        while (current->next != end) 
+        {
+            if (strcmp(current->lname, (current->next)->lname) > 0) 
+            {
+                temp = current->next;
+                previous->next = temp;
+                current->next = temp->next;
+                temp->next = current;
+                current = temp;
+            }
+            previous = current;
+            current = current->next;
+        }
+        end = current;
+    }
     return EXIT_SUCCESS;
 }
 
@@ -282,7 +339,7 @@ int read_from_file(char* filename)
 {
     FILE* fp = NULL;
     person new_head = { "","",0, NULL };
-    char current_fname[32], current_lname[32];
+    char current_fname[MAX_NAME_SIZE], current_lname[MAX_NAME_SIZE];
     int current_birth_year;
 
     fp = fopen(filename, "r");
